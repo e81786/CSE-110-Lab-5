cf4fa0c6b45d074386deb4d1f2f970dccf151e68
Eric Dong <e1dong@ucsd.edu>
2023-05-07 20:44:23 -0700

    Finish most of lab 5

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,59 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  // TODO
+  const speak = document.getElementById("text-to-speak");
+  const options = document.getElementById("voice-select");
+  const smile = document.getElementsByTagName("img")[0];
+  const btn = document.getElementsByTagName("button")[0];
+  var synth = window.speechSynthesis;
+  var msg = new SpeechSynthesisUtterance();
+
+  var timer = setInterval(function () {
+    const voices = speechSynthesis.getVoices();
+    if (voices.length !== 0) {
+      for (let i = 0; i < voices.length; i++) {
+        var newOption = document.createElement("option");
+        newOption.textContent = `${voices[i].name} (${voices[i].lang})`;
+        newOption.setAttribute("data-lang", voices[i].lang);
+        newOption.setAttribute("data-name", voices[i].name);
+        newOption.setAttribute("value", i);
+        options.appendChild(newOption);
+      }
+
+      speak.onchange = function () {
+        msg.text = speak.value;
+      };
+      options.onchange = function () {
+        msg.voice = voices[options.value];
+      };
+      msg.volume = 1;
+
+      clearInterval(timer);
+    }
+  }, 200);
+
+  btn.addEventListener("click", () => {
+    speechSynthesis.speak(msg);
+    setTimeout(function () {
+      let i = 0;
+      if (synth.speaking) {
+        var speaketh = setInterval(function () {
+          if (!synth.speaking) {
+            clearInterval(speaketh);
+          }
+          if (i % 2 == 0) {
+            smile.src = "/assets/images/smiling-open.png";
+          } else {
+            smile.src = "/assets/images/smiling.png";
+          }
+          if (!synth.speaking) {
+            clearInterval(speaketh);
+          }
+          i++;
+        }, 200);
+      }
+    }, 250);
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,69 @@
 // expose.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  // TODO
+  const hornSelector = document.getElementById("horn-select");
+  const hornImage = document.getElementsByTagName("img")[0];
+  const hornAudio = document.getElementsByTagName("audio")[0];
+  const volume = document.getElementById("volume");
+  const volumeImage = document.getElementsByTagName("img")[1];
+  const playBtn = document.getElementsByTagName("button")[0];
+  const jsConfetti = new JSConfetti();
+  hornAudio.volume = 0.5;
+
+  hornSelector.onchange = function () {
+    let hornValue = hornSelector.value;
+    if (hornValue == "air-horn") {
+      hornImage.src = "/assets/images/air-horn.svg";
+      hornAudio.src = "/assets/audio/air-horn.mp3";
+      hornImage.alt = "Air Horn Image";
+      hornAudio.alt = "Air Horn Audio";
+    } else if (hornValue == "car-horn") {
+      hornImage.src = "/assets/images/car-horn.svg";
+      hornAudio.src = "/assets/audio/car-horn.mp3";
+      hornImage.alt = "Car Horn Image";
+      hornAudio.alt = "Car Horn Audio";
+    } else if (hornValue == "party-horn") {
+      hornImage.src = "/assets/images/party-horn.svg";
+      hornAudio.src = "/assets/audio/party-horn.mp3";
+      hornImage.alt = "Party Horn Image";
+      hornAudio.alt = "Party Horn Audio";
+    }
+  };
+
+  volume.onchange = function () {
+    let volLevel = volume.value;
+    if (volLevel == 0) {
+      volumeImage.src = "/assets/icons/volume-level-0.svg";
+      volumeImage.alt = "Muted";
+    } else if (volLevel < 33) {
+      volumeImage.src = "/assets/icons/volume-level-1.svg";
+      volumeImage.alt = "Volume level 1";
+    } else if (volLevel < 67) {
+      volumeImage.src = "/assets/icons/volume-level-2.svg";
+      volumeImage.alt = "Volume level 2";
+    } else {
+      volumeImage.src = "/assets/icons/volume-level-3.svg";
+      volumeImage.alt = "Volume level 3";
+    }
+    hornAudio.volume = volLevel / 100;
+  };
+
+  playBtn.addEventListener("click", () => {
+    if (hornAudio.getAttribute("src")) {
+      if (hornAudio.paused) {
+        hornAudio.play();
+      } else {
+        hornAudio.pause();
+        hornAudio.currentTime = 0;
+        hornAudio.play();
+      }
+      console.log(hornAudio.volume);
+    }
+    let hornValue = hornSelector.value;
+    if (hornValue == "party-horn") {
+      jsConfetti.addConfetti();
+    }
+  });
 }
